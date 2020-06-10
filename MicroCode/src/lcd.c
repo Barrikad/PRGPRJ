2c44d252239a5f7f3d98fb23b754524358e34be8
Mads Marquart <madsmtm@gmail.com>
2020-06-10 16:56:56 +0200

    Add lcdWriteChar and helper defines

--- MicroCode/src/lcd.c
+++ MicroCode/src/lcd.c
@@ -1,5 +1,8 @@
 #include "lcd.h"
 
+#define LCD_WIDTH 128
+#define LCD_HEIGHT 32
+
 static uint8_t buffer[512];
 
 void lcdInit() {
@@ -16,27 +19,37 @@ void lcdFlush() {
 	lcd_push_buffer(buffer);
 }
 
-// Write
+// Write a character into the buffer at the specified location.
+// Letting parts of the character go outside the screen is allowed.
+// TODO: Make `y` work in this.
+void lcdWriteChar(char chr, int8_t x, int8_t y) {
+	int8_t i;
+	// Note: We don't detect overflow here!
+	const char * charData = character_data[chr - 0x20];
+	for (i = 0; i < 5; i++) {
+		// Don't write anything if the position is outside the screen
+		// The fact that `x` is signed is desired, this lets us implement scrolling text easily.
+		if (x + i < 0 || x + i >= LCD_WIDTH) {
+			continue;
+		}
+		buffer[x + i] = charData[i];
+	}
+}
+
 void lcdWriteString(char * string, uint8_t line, uint8_t slice) {
-	uint8_t i, j;
-	const char * charData;
-	// Clear buffer
-	lcdClear();
+	uint8_t i;
 	// Loop over each character in the string
 	for (i = 0; string[i]; i++) {
-		charData = character_data[string[i] - 0x20];
-		for (j = 0; j < 5; j++) {
-			buffer[line*128 + slice + i * 6 + j] = charData[j];
-		}
+		lcdWriteChar(string[i], line*LCD_WIDTH + slice + i * 6, 0);
 	}
 }
 
 void lcdUpdate() {
     for (int i = 0; i < 4; i++) {
-        uint8_t first = buffer[i*128];
-        for (int j = 0; j < 127; j++) {
-            buffer[i*128 + j] = buffer[i*128 + j + 1];
+        uint8_t first = buffer[i*LCD_WIDTH];
+        for (int j = 0; j < LCD_WIDTH - 1; j++) {
+            buffer[i*LCD_WIDTH + j] = buffer[i*LCD_WIDTH + j + 1];
         }
-        buffer[i*128 + 127] = first;
+        buffer[i*LCD_WIDTH + LCD_WIDTH - 1] = first;
     }
 }
