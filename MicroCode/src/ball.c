a5aba3c080bfc1f4e00eadb030fe3c741dfa8e83
GustavLeth <s173975@student.dtu.dk>
2020-06-10 16:04:47 +0200

    Dokumentaion for opgave 4, ball

--- MicroCode/src/ball.c
+++ MicroCode/src/ball.c
@@ -5,6 +5,7 @@
 #define BOTTOM 21
 #define SIZE 20
 
+// Sets up the initial values to make the function run.
 void startBall(){
     drawWindow();
     ball_t ball = {(6 << 14) , (6 << 14) , (1 << 14) + (5 << 13), (2 << 14) };
@@ -21,6 +22,7 @@ void startBall(){
     }
 }
 
+// Draws the window for the ball and counter.
 void drawWindow(){
     walls(LEFT,TOP,RIGHT,BOTTOM);
     int8_t sTOP = (TOP + BOTTOM)/2 - 1;
@@ -32,21 +34,32 @@ void drawWindow(){
     printf("Hits: 0");
 }
 
+// Moves the ball in direction of the velocity
+// Parameteres:     *b: a ball.
 void moveBall(ball_t *b){
     (*b).x += (*b).vx;
     (*b).y += (*b).vy;
 }
 
+
+// Draws the ball on the terminal
+// Parameters:      *b a ball.
 void drawBall(ball_t *b){
     gotoxy(((*b).x >> 14) + LEFT,((*b).y >> 14)/2 + TOP);
     printf("o");
 }
 
+// Removes the drawing of the ball
+// Parameters:      *B a ball.
 void undrawBall(ball_t *b){
     gotoxy(((*b).x >> 14) + LEFT,((*b).y >> 14)/2 + TOP);
     printf(" ");
+    // The function draws an empty space to remove the ball from the last location.
 }
 
+// Update the balls position on terminal
+// Parameters:      *b a ball.
+//                  *collisions a counter
 void updateBall(ball_t *b, uint16_t * collisions){
     undrawBall(b);
     moveBall(b);
@@ -57,7 +70,6 @@ void updateBall(ball_t *b, uint16_t * collisions){
     	(*collisions)++;
         bounceBall(b,collision);
         moveBall(b);
-        // Print updated collision amount
         int8_t sTOP = (TOP + BOTTOM)/2 - 1;
         int8_t sLEFT = (LEFT + RIGHT)/2 - 6;
         gotoxy(sLEFT + 7, sTOP + 1);
@@ -65,8 +77,16 @@ void updateBall(ball_t *b, uint16_t * collisions){
     }
 
     drawBall(b);
+    // The function removes the previous ball, then updates the ball position.
+    // Then checks if there has been a collision. If there has been a collision'
+    // the balls velocity is change, and its position is then updated accordingly.
+    // The collision counter on the screen us update. The ball is drawn in its new
+    // position.
 }
 
+
+// Check which wall the ball collides with.
+// Parameters:      *b a ball.
 int8_t ballCollides(ball_t *b){
     int8_t collision = 0;
     if((*b).x <= 0){
@@ -84,6 +104,9 @@ int8_t ballCollides(ball_t *b){
     return collision;
 }
 
+// Changes the velocity of the ball
+// Parameters:      *b a ball.
+//                  collision a bit value
 void bounceBall(ball_t *b, int8_t collision){
     if(collision & 5){
         (*b).vx = -(*b).vx;
@@ -91,4 +114,7 @@ void bounceBall(ball_t *b, int8_t collision){
     if(collision & 10){
         (*b).vy = -(*b).vy;
     }
+
+    // The function takes the ball and the collision and change the velocity
+    // of the ball according to which wall was hit.
 }
