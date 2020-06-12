b5b096025552fb5c6c6ff8aa0b09f83c3948de95
Mads Marquart <madsmtm@gmail.com>
2020-06-12 14:51:58 +0200

    Add LCD files

---
+++ MicroCode/HAL_inc/lcd.h
@@ -0,0 +1,28 @@
+#include <stdint.h>
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#define LCD_WIDTH 128
+#define LCD_HEIGHT 32
+
+// Initialize the LCD.
+void lcdInit();
+
+// Clear the internal LCD buffer.
+void lcdClear();
+
+// Write a single pixel to the buffer at the specified location.
+void lcdWritePixel(uint8_t x, uint8_t y);
+
+// Clear a single pixel in the buffer at the specified location.
+void lcdClearPixel(uint8_t x, uint8_t y);
+
+// Write a character into the buffer at the specified location.
+// TODO: Move this to API?
+void lcdWriteChar(char chr, uint8_t x, uint8_t y);
+
+// Flush the internal buffer to the LCD.
+void lcdFlush();
+
+#endif /* LCD_H_ */
---
+++ MicroCode/HAL_src/lcd.c
@@ -0,0 +1,46 @@
+#include "lcd.h"
+#include "30010_io.h"
+#include "charset.h"
+#include <string.h>
+
+#define LCD_BUFFER_SIZE 512
+
+// The internal buffer.
+static uint8_t buffer[LCD_BUFFER_SIZE];
+
+void lcdInit() {
+    lcd_init();
+}
+
+void lcdClear() {
+    memset(buffer, 0x00, LCD_BUFFER_SIZE);
+}
+
+void lcdWritePixel(uint8_t x, uint8_t y) {
+    buffer[x + (y / 8) * LCD_WIDTH] |=  (0x01 << y);
+}
+
+void lcdClearPixel(uint8_t x, uint8_t y) {
+    buffer[x + (y / 8) * LCD_WIDTH] &= ~(0x01 << y);
+}
+
+// Internal helper to write a vertical line to the buffer.
+// TODO: Make this public?
+static void lcdWriteVerticalLine(uint8_t line, uint8_t x, uint8_t y) {
+    // TODO: Make `y` work properly here!
+    buffer[x + (y / 8) * LCD_WIDTH] = line;
+}
+
+// TODO: Make `y` work properly in this.
+void lcdWriteChar(char chr, uint8_t x, uint8_t y) {
+    int8_t i;
+    // Note: We don't detect overflow here!
+    const char * charData = character_data[chr - 0x20];
+    for (i = 0; i < 5; i++) {
+        lcdWriteVerticalLine(charData[i], x + i, y);
+    }
+}
+
+void lcdFlush() {
+    lcd_push_buffer(buffer);
+}
