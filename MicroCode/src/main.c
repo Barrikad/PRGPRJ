990e7bddd28d4eb54897f8d37343960a44c75b0a
Barrikad <s184459@student.dtu.dk>
2020-06-12 14:55:52 +0200

    drawing program in main.c

--- MicroCode/src/main.c
+++ MicroCode/src/main.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "stm32f30x_conf.h" // STM32 config
+#include "30010_io.h"
+#include "stm32f30x.h"
+#include "joystick.h"
+#include "ansi.h"
 
 
 int main(void)
 {
     uart_init(115200);
-
+    clrscr();
+    initJoystick();
     while(1){
+        for(uint16_t i = 1; i > 0 ; i++){}
 
+        if(readJoystick(JOYSTICK_DOWN))
+            down(1);
+        if(readJoystick(JOYSTICK_LEFT))
+            left(1);
+        if(readJoystick(JOYSTICK_RIGHT))
+            right(1);
+        if(readJoystick(JOYSTICK_UP))
+            up(1);
+        if(readJoystick(JOYSTICK_MIDDLE)){
+            printf("X");
+            left(1);
+        }
     }
 }
