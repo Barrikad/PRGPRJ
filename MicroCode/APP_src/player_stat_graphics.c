9d1bfcbe4fa6735bbb827f9b7ff498e894726aea
GustavLeth <s173975@student.dtu.dk>
2020-06-22 12:37:58 +0200

    Score and lives on LCD

--- MicroCode/APP_src/player_stat_graphics.c
+++ MicroCode/APP_src/player_stat_graphics.c
@@ -10,32 +10,41 @@ static void getScore(char score[3][14], player_t* players, uint8_t numPlayers){
     }
 }
 
+static void getLives(char numLives[3], player_t* players, uint8_t numPlayers){
+    for (int i = 0; i < numPlayers; i++) {
+        sprintf(numLives, "%d",players[i].lives);
+    }
+}
+
 void livesAndScoreLcd(player_t* players, uint8_t numPlayers) {
-    char* header = {"        Score:  Lives:"};
+    char* header = {"         Score: Lives:"};
     char* playerName[] = {"Player 1", "Player 2", "Player 3"};
     char score[3][14];
+    char numLives[3];
     lcdClear();
 
     for (int i = 0; i < strlen(header); i++) {
         lcdWriteChar(header[i], i * 6, 0);
     }
 
-    for (int i = 1; i < (numPlayers + 1); i++) {
-        for (int j = 0; j < strlen(playerName[i-1]); j++) {
-            lcdWriteChar(playerName[i-1][j], j * 6, i * 8);
+    for (int i = 0; i < numPlayers; i++) {
+        for (int j = 0; j < strlen(playerName[i]); j++) {
+            lcdWriteChar(playerName[i][j], j * 6, (i+1) * 8);
         }
     }
 
     getScore(score, players, numPlayers);
 
-    for (int i = 1; i < (numPlayers + 1); i++) {
-        for (int j = 9; j < 15; j++) {
-                lcdWriteChar(score[i][j], j * 6, i * 8);
+    for (int i = 0; i < numPlayers; i++) {
+        for (int j = 0; j < 6; j++) {
+                lcdWriteChar(score[i][j], (j + 9) * 6, (i + 1) * 8);
         }
     }
 
-    for (int i = 1; i < (numPlayers + 1); i++) {
-            lcdWriteChar(players[i].lives, 20 * 6, i * 8);
+    getLives(numLives, players, numPlayers);
+
+    for (int i = 0; i < numPlayers; i++) {
+            lcdWriteChar(numLives[i], 20 * 6, (i+1) * 8);
     }
 
     lcdFlush();
