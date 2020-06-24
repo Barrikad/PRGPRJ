f51b90c62f9a802243b145f9d9c47dea8b5d499e
Mads Marquart <madsmtm@gmail.com>
2020-06-24 15:27:54 +0200

    Fix ansi functions using signed integers instead of unsigned

--- MicroCode/API_inc/ansi.h
+++ MicroCode/API_inc/ansi.h
@@ -27,28 +27,28 @@ void cursorHide();
 void cursorShow();
 
 //go to position x,y. x is width, y is height. 0-indexed
-void cursorToXY(int8_t x, int8_t y);
+void cursorToXY(uint8_t x, uint8_t y);
 
 //underline newly written text
-void underline(int8_t on);
+void underline(uint8_t on);
 
 //blink newly written text
-void blink(int8_t on);
+void blink(uint8_t on);
 
 //inverse background and foreground color for newly written text
-void inverse(int8_t on);
+void inverse(uint8_t on);
 
 //go up x lines
-void cursorUp(int8_t x);
+void cursorUp(uint8_t x);
 
 //go down x lines
-void cursorDown(int8_t x);
+void cursorDown(uint8_t x);
 
 //go right x columns
-void cursorRight(int8_t x);
+void cursorRight(uint8_t x);
 
 //go left x columns
-void cursorLeft(int8_t x);
+void cursorLeft(uint8_t x);
 
 void printFix(int32_t i);
 
